de1798ce63e2c0fd580d5d56b43d6eca6c6722ac
Alberto Delgado <alberto.delgado.cabrera@walmart.com>
2025-04-06 23:49:26 -0700

    typo

--- web/modules/Homepage/HowItWorks.tsx
+++ web/modules/Homepage/HowItWorks.tsx
@@ -3,10 +3,10 @@ import { GitHubComment } from '@/components/GitHubComment/GitHubComment';
 export const HowItWorks = () => {
   return (
     <GitHubComment
-      username="Bimba"
-      description="This is an example comment."
-      minutesAgo={5}
-      avatarUrl="https://github.com/mantinedev/mantine/blob/master/.demo/avatars/avatar-2.png?raw=true"
+    // username="Bimba"
+    // description="This is an example comment."
+    // minutesAgo={5}
+    // avatarUrl="https://github.com/mantinedev/mantine/blob/master/.demo/avatars/avatar-2.png?raw=true"
     />
   );
 };
